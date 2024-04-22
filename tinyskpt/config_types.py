add18135d791b24ea4a7919b6c578e3569b6028a
Zhuyi Xue <zhuyi.xue@alum.utoronto.ca>
2024-04-22 21:28:31 +0000

    implemented loading of config

---
+++ tinyskpt/config_types.py
@@ -0,0 +1,63 @@
+"""Configuration related classes."""
+
+import pydantic
+
+
+class ArchConfig(pydantic.BaseModel):
+    """Configurations related to the model architecture."""
+
+    # Size of the vocabulary.
+    vocab_size: int
+    # Number of training examples per batch.
+    batch_size: int
+    # Size of the embedding vectors.
+    embed_size: int
+    # Number of attention heads.
+    num_heads: int
+    # Number of tokens in each context.
+    context_length: int
+    # Number of attention layers. An attention layer has two sublayers: a
+    # multi-head attention sublayer & a feedforward layer.
+    num_layers: int
+    # Dropout rate.
+    dropout_rate: float
+
+    @property
+    def head_size(self) -> int:
+        """Size of the attention head
+
+        Aka. the dimension of key, query, or value. It's derived from the
+        embed_size and num_heads.
+        """
+        return self.embed_size // self.num_heads
+
+
+class TrainConfig(pydantic.BaseModel):
+    """Configurations related to model training."""
+
+    # The maximum number of iterations to run the training loop.
+    max_iters: int
+    # Learning rate.
+    learning_rate: float
+
+
+class EvalConfig(pydantic.BaseModel):
+    """Configurations related to model evaluation.
+
+    Evaluation is aka. validation.
+    """
+
+    # How often to evaluate the loss on the train and validation sets.
+    eval_interval: int
+    # How many batches to average over when estimating the loss.
+    num_batches_for_eval: int
+    # Fraction of data to use for validation, between 0 and 1.
+    eval_size: float
+
+
+class Config(pydantic.BaseModel):
+    """Overall configuration object."""
+
+    arch_config: ArchConfig
+    train_config: TrainConfig
+    eval_config: EvalConfig
--- tinyskpt/train.py
+++ tinyskpt/train.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 
+import yaml
+
+from tinyskpt import config_types
+
+
+def _load_config(path: Path) -> config_types.Config:
+    return config_types.Config.model_validate(yaml.safe_load(path.read_text()))
+
 
 def train(config_path: Path | None = None, output_dir: Path | None = None) -> None:
     """Train a transformer.
@@ -11,8 +19,13 @@ def train(config_path: Path | None = None, output_dir: Path | None = None) -> No
     """
     output_dir = output_dir or Path.cwd()
     config_path = (
-        config_path or (Path("__file__").parent / "data/default_config.yaml").resolve()
+        config_path or (Path(__file__).parent / "data/default_config.yaml").resolve()
     )
-    print("TODO: Implement training logic.")
+
     print(f"{output_dir=:}")
     print(f"{config_path=:}")
+
+    config = _load_config(config_path)
+    print(f"{config=:}")
+
+    print("TODO: Implement training logic.")
