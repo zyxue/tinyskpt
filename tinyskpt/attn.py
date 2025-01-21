bug fix for scaling weight by sqrt root of head size instead

tinyskpt/attn.py
@@ -55,7 +55,7 @@ class SingleHeadAttention(nn.Module):
 
         key_transpose = key.transpose(dim0=1, dim1=2)  # (B, C, H) -> (B, H, C)
         weight = query @ key_transpose  # (B, C, H) @ (B, H, C) -> (B, C, C)
-        weight /= embed_size**0.5
+        weight /= head_size**0.5   # Scale by sqrt root of head size according to the attn paper.
         weight = weight.masked_fill(
             self.tril[:context_length, :context_length] == 0, float("-inf")
         )